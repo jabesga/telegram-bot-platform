7e5bf0a5cb0da3533457a6eeb92be0dcc99173f9
Jon Ander Besga <jonan.bsg@gmail.com>
2016-06-04 17:23:03 +0200

    updated domain

--- app/__init__.py
+++ app/__init__.py
@@ -2,7 +2,7 @@ from flask import Flask
 from flask_socketio import SocketIO
 
 app = Flask(__name__, static_folder='static/')
-app.DOMAIN = 'https://telegrambot-pyframework-jabesga.c9users.io'
+app.DOMAIN = 'https://telegrambot-plataform.herokuapp.com'
 socketio = SocketIO(app)
 
 from .core import views
